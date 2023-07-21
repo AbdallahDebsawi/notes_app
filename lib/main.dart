0a9488b0ba6a3948dc6b0366306b8487d84272b4
abdallah <abdallahde503@gmail.com>
2023-07-21 19:51:04 +0300

    refactoring Bloc Provider

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ import 'constant.dart';
 void main() async {
   await Hive.initFlutter();
   Bloc.observer = SimpleBLocObserver();
-  await Hive.openBox<NoteModel>(kNoteBox);
+  //you have to register the adapter before you use it
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNoteBox);
   runApp(const NotesApp());
 }
 
@@ -21,17 +22,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddNoteCubit(),
-        )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotesView(),
     );
   }
 }
